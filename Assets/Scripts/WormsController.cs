e0a388c71b650dcceb3edfb573d5a24bbacfb4f8
Stefanie Hubmer <stefanie.hubmer@htl-salzburg.ac.at>
2020-02-05 11:18:37 +0100

    second Player

--- Assets/Scripts/WormsController.cs
+++ Assets/Scripts/WormsController.cs
@@ -9,6 +9,10 @@ public class WormsController : MonoBehaviour
     public float moveforce;
     public Rigidbody rb2;
     public float ForceSphere;
+    public Rigidbody rb3;
+    public Rigidbody rb4;
+    public Transform SpawnPoint;
+    public Transform SpawnPoint2;
 
 
     void Update()
@@ -21,26 +25,51 @@ public class WormsController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Vector3 xsidewards = new Vector3(moveforce, 0, 0);
+            Vector3 xsidewards = new Vector3(-moveforce, 0, 0);
             rb.AddForce(xsidewards);
         }
 
         if(Input.GetKey(KeyCode.A))
         {
-            Vector3 x2sidewards = new Vector3(-moveforce, 0, 0);
+            Vector3 x2sidewards = new Vector3(moveforce, 0, 0);
             rb.AddForce(x2sidewards);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Vector3 PositionSphere = new Vector3(0.5f, 0.5f, 0);
+            Vector3 Position = SpawnPoint2.position;
             Rigidbody clone;
-            clone = Instantiate(rb2, transform.position + PositionSphere, transform.rotation);
+            clone = Instantiate(rb2, Position, transform.rotation);
             clone.velocity = transform.TransformDirection(ForceSphere, 0f,0f);
 
             Vector3 Shootingforce = new Vector3(ForceSphere, 0, 0);
             rb2.AddForce(Shootingforce);
+        }
 
-            
+        //Second Player
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Vector3 force = new Vector3(0, jumpforce, 0);
+            rb3.AddForce(force);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            Vector3 forcesidewards = new Vector3(-moveforce, 0, 0);
+            rb3.AddForce(forcesidewards);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            Vector3 forcesidewards = new Vector3(moveforce, 0, 0);
+            rb3.AddForce(forcesidewards);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Vector3 Player2Position = SpawnPoint.position;
+            Rigidbody clone;
+            clone = Instantiate(rb4, Player2Position, transform.rotation);
+            clone.velocity = transform.TransformDirection(ForceSphere, 0f, 0f);
+
+            Vector3 Shootingforce = new Vector3(ForceSphere, 0, 0);
+            rb4.AddForce(Shootingforce);
         }
     }
 }
